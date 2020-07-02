7280fd6f27f5126c67d4a487b8f92103e7b72c93
1065196118 <67002064+1065196118@users.noreply.github.com>
2020-07-02 21:55:24 +0200

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,38 @@
+Students_names=['yasser','bader','Ahmad','shabaan','Hassan','jelal',
+                'Omar','Mahmud','Mazen','Ossama','Ali','Abdullah',
+                'Noor','Mariam','Alia','Hind','Israa','Mohammed','Zain','Shirin']
+print("The last ten names",Students_names[10: ])
+for i in Students_names:
+    print(i)
+# =============================================================================
+# =============================================================================
+print("append")
+# Students_names.append(['Hussain','Adel','Regep','Waleed'])
+Students_names.append('Hussain')
+Students_names.append('Adel')
+Students_names.append('Regep')
+Students_names.append('Waleed')
+print(Students_names)
+# =============================================================================
+# =============================================================================
+print("Nested Dictionary")
+dic1={'name':'yasser','age':19,'date of birth':2001,'class':'first'}
+dic2={'name':'bader','age':40,'date of birth':1888,'class':'second'}
+dic3={'name':'Ahmad','age':25,'date of birth':1991,'class':'three'}
+dic4={'name':'shabaan','age':30,'date of birth':1885,'class':'Fourth'}
+dic5={'name':'Hassan','age':18,'date of birth':2002,'class':'three'}
+dic6={'name':'jelal','age':20,'date of birth':1999,'class':'first'}
+dic7={'name':'Omar','age':22,'date of birth':1997,'class':'Fourth'}
+dic8={'name':'Mahmud','age':21,'date of birth':1998,'class':'three'}
+dic9={'name':'Mazen','age':15,'date of birth':2005,'class':'second'}
+dic10={'name':'Ossama','age':45,'date of birth':1898,'class':'Fourth'}
+print("the name= ",dic1['name'],"\nthe class= ",dic1['class'])
+print("the name= ",dic2['name'],"\nthe class= ",dic2['class'])
+print("the name= ",dic3['name'],"\nthe class= ",dic3['class'])
+print("the name= ",dic4['name'],"\nthe class= ",dic4['class'])
+print("the name= ",dic5['name'],"\nthe class= ",dic5['class'])
+print("the name= ",dic6['name'],"\nthe class= ",dic6['class'])
+print("the name= ",dic7['name'],"\nthe class= ",dic7['class'])
+print("the name= ",dic8['name'],"\nthe class= ",dic8['class'])
+print("the name= ",dic9['name'],"\nthe class= ",dic9['class'])
+print("the name= ",dic10['name'],"\nthe class= ",dic10['class'])
